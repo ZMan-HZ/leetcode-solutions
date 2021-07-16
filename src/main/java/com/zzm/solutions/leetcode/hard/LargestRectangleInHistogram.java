f469f937c150e6d17564691c19cd46b77eded179
zman-hz <manzhzh@163.com>
2021-07-16 23:24:00 +0800

    feat(leetcode): add solution about largest rrectangle

--- src/main/java/com/zzm/solutions/leetcode/hard/LargestRectangleInHistogram.java
+++ src/main/java/com/zzm/solutions/leetcode/hard/LargestRectangleInHistogram.java
@@ -1,6 +1,9 @@
 package com.zzm.solutions.leetcode.hard;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.Objects;
+import java.util.Stack;
 
 /**
  * <b>柱状图中最大的矩形</b>
@@ -55,10 +58,54 @@ public class LargestRectangleInHistogram {
         return max;
     }
 
+    /**
+     * 思路：
+     * 单调栈
+     * 操作规则（下面都以单调递增栈为例）
+     * 1. 如果新的元素比栈顶元素大，就入栈
+     * 2. 如果新的元素较小，那就一直把栈内元素弹出来，直到栈顶比新元素小
+     * 加入这样一个规则之后，会有什么效果
+     * 1. 栈内的元素是递增的
+     * 2. 当元素出栈时，说明这个新元素是出栈元素向后找第一个比其小的元素
+     * 举个例子，现在索引在 6 ，栈里是 1 5 6 。
+     * 接下来新元素是 2 ，那么 6 需要出栈。
+     * 当 6 出栈时，右边 2 代表是 6 右边第一个比 6 小的元素。
+     * 3. 当元素出栈后，说明新栈顶元素是出栈元素向前找第一个比其小的元素
+     * 当 6 出栈时，5 成为新的栈顶，那么 5 就是 6 左边第一个比 6 小的元素。
+     *
+     * @param heights
+     * @return
+     */
+    public static int maxRectangle(int[] heights) {
+        int max = 0;
+        if (ArrayUtils.isEmpty(heights)) {
+            return max;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int length = heights.length;
+        int[] temp = new int[length + 2];
+        //为了避免是单调递增的数组入栈，并且让入栈的height能全部出栈，在数组首位各补一个 0
+        System.arraycopy(heights, 0, temp, 1, length);
+        for (int index = 0; index < temp.length; index++) {
+            int current = temp[index];
+            while (!stack.isEmpty() && temp[stack.peek()] > current) {
+                int peek = stack.pop();
+                int left = stack.peek();
+                max = Math.max(max, (index - left - 1) * temp[peek]);
+            }
+            stack.push(index);
+        }
+
+        return max;
+    }
+
 
     public static void main(String[] args) {
         int[] heights = {2, 1, 5, 6, 2, 3};
         int max = max(heights);
+        System.out.println(max);
+
+        max = maxRectangle(heights);
         System.out.println(max);
     }
 }
