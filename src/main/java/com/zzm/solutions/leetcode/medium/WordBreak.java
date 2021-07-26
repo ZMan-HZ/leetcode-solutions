bb42856e1f2ab3f3f31e31f78911e41916885057
zman-hz <manzhzh@163.com>
2021-07-26 22:22:01 +0800

    feat(leetcode): add solution about word break

--- src/main/java/com/zzm/solutions/leetcode/medium/WordBreak.java
+++ src/main/java/com/zzm/solutions/leetcode/medium/WordBreak.java
@@ -1,6 +1,10 @@
 package com.zzm.solutions.leetcode.medium;
 
+import com.google.common.collect.Lists;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * <b>单词拆分</b>
@@ -33,7 +37,21 @@ public class WordBreak {
 
 
     /**
-     * 思路：
+     * 思路：动态规划
+     * 定义 dp[i] 表示字符串 s 前 i 个字符组成的字符串s[0..i−1] 是否能被空格拆分成若干个字典中出现的单词。
+     * 从前往后计算考虑转移方程，每次转移的时候我们需要枚举包含位置 i−1 的最后一个单词，
+     * 看它是否出现在字典中以及除去这部分的字符串是否合法即可。
+     * 公式化来说，我们需要枚举 s[0..i−1] 中的分割点 j ，看 s[0..j−1] 组成的字符串 s1 （默认j=0 时 s1为空串）
+     * 和s[j..i−1] 组成的字符串 s2 是否都合法，如果两个字符串均合法，那么按照定义 s1 和 s2 拼接成的字符串也同样合法。
+     * 由于计算到dp[i] 时我们已经计算出了 dp[0..i−1] 的值，
+     * 因此字符串 s1 是否合法可以直接由 dp[j] 得知，剩下的我们只需要看 s2是否合法即可，
+     * 因此我们可以得出如下转移方程：
+     * dp[i]= dp[j] && check(s[j..i−1])
+     * 其中 check(s[j..i−1]) 表示子串 s[j..i−1] 是否出现在字典中。
+     * 对于检查一个字符串是否出现在给定的字符串列表里一般可以考虑哈希表来快速判断，
+     * 同时也可以做一些简单的剪枝，枚举分割点的时候倒着枚举，如果分割点 j 到 i 的长度已经大于字典列表里最长的单词的长度，
+     * 那么就结束枚举，但是需要注意的是下面的代码给出的是不带剪枝的写法。
+     * 对于边界条件，我们定义 dp[0]=true 表示空串且合法。
      *
      * @param s
      * @param wordDict
@@ -41,21 +59,42 @@ public class WordBreak {
      */
     public static boolean wordBreak(String s, List<String> wordDict) {
 
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+       /* int maxw = 0;
+        int minw = len + 1;
+        Set<String> set = new HashSet();
+        for (String str : wordDict) {
+            set.add(str);
+            maxw = Math.max(maxw, str.length());
+            minw = Math.min(minw, str.length());
+        }*/
+       //其实stream操作更慢
+        Set<String> dict = new HashSet(wordDict);
+        int max = dict.stream().map(String::length).max(Integer::compareTo).orElse(len);
+        int min = dict.stream().map(String::length).min(Integer::compareTo).orElse(0);
 
-        for (String word : wordDict) {
-            //先找开头就可以组成的单词
-            if (s.startsWith(word)) {
-                //开头就包含的单词，移除掉后，应该还可以继续拆分
-
+        for (int index = 1; index < len + 1; index++) {
+            //dp[i]只需要往前探索到词典里最长的单词即可
+            for (int kndex = index; kndex >= 0 && kndex >= index - max; kndex--) {
+                //如果取的字符串小于最短的单词，不需要判断set是否有
+                if (index - kndex < min) {
+                    continue;
+                }
+                if (dp[kndex] && dict.contains(s.substring(kndex, index))) {
+                    dp[index] = true;
+                    break;
+                }
             }
-
-
         }
-
-        return false;
+        return dp[len];
     }
 
     public static void main(String[] args) {
-
+        String s = "leetcode";
+        List<String> dict = Lists.newArrayList("leet", "code");
+        boolean wordBreak = wordBreak(s, dict);
+        System.out.println("wordBreak = " + wordBreak);
     }
 }
---
+++ src/main/java/com/zzm/solutions/present/ThreadPrinter.java
@@ -0,0 +1,49 @@
+package com.zzm.solutions.present;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 题目：多线程(某里云)
+ * <p>
+ * 实现两个线程使之交替打印1 -100：
+ * 如：两个线程分别为Printer1 和Printer2，
+ * 最后输出的结果：
+ * Printer1 - 1；
+ * Printer2 - 2；
+ * Printer1 - 3；
+ * Printer2 - 4；
+ *
+ * @author Zhenzhen
+ * @version v1.0.0
+ * @since 2021/7/26 星期一
+ */
+public class ThreadPrinter {
+
+    private static AtomicInteger counter = new AtomicInteger(1);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (counter.get() < 100) {
+                    synchronized (this) {
+                        try {
+                            this.notifyAll();
+                            System.out.println(Thread.currentThread().getName() + "\t->\t" + counter.getAndIncrement());
+                            this.wait();
+                        } catch (InterruptedException e) {
+                            Thread.interrupted();
+                        }
+                    }
+                }
+            }
+        };
+        Thread printer1 = new Thread(runnable, "Printer1");
+        Thread printer2 = new Thread(runnable, "Printer2");
+        printer1.start();
+        Thread.sleep(10);
+        printer2.start();
+    }
+
+}
